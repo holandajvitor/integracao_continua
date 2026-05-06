72c55dd64b63ade72e97d1aa6042d806a6c0a94c
holandajvitor <holandajvitor@gmail.com>
2026-05-06 20:37:36 -0300

    commit inicial

---
+++ .history/database/db_20260421145944.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"log"
+	"os"
+	"github.com/guilhermeonrails/api-go-gin/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	DB  *gorm.DB
+	err error
+)
+
+func ConectaComBancoDeDados() {
+	endereco := os.Getenv("DB_HOST")
+	usuario := os.Getenv("DB_USER")
+	senha := os.Getenv("DB_PASSWORD")
+	nomeBanco := os.Getenv("DB_NAME")
+	portaBanco := os.Getenv("DB_PORT")
+	stringDeConexao := "host=postgres user=root password=root dbname=root port=5432 sslmode=disable"
+	DB, err = gorm.Open(postgres.Open(stringDeConexao))
+	if err != nil {
+		log.Panic("Erro ao conectar com banco de dados")
+	}
+
+	DB.AutoMigrate(&models.Aluno{})
+}
